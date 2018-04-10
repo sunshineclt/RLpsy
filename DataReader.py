9d2f20a9f628c6f962e0a144f8cd0fc6c9b4aff6
Zac Chan <sunshinecltzac@gmail.com>
2018-04-10 15:17:15 +0800

    data reader change

--- DataReader.py
+++ DataReader.py
@@ -1,11 +1,31 @@
 import csv
 import json
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    rawFile = open("data/0_2018_Apr_09_2139_quit.csv", "r")
+    rawFile = open("data/0_2018_Apr_09_2222.csv", "r")
     reader = csv.DictReader(rawFile, delimiter="#")
+    all_data = []
     for row in reader:
-        data = row["trial_data"]
-        if data != "--":
-            transformed_data = json.loads(data)
-            print(transformed_data)
+        trial = row["trial_data"]
+        if trial != "--":
+            transformed_data = json.loads(trial)
+            # print(transformed_data)
+            all_data.append(transformed_data)
+
+    steps = []
+    times = []
+    average = []
+    for trial in all_data:
+        steps.append(len(trial))
+        times.append(0)
+        for step in trial:
+            times[-1] += step[3]
+        average.append(times[-1] / steps[-1])
+
+    plt.plot(steps)
+    plt.show()
+    plt.plot(times)
+    plt.show()
+    plt.plot(average)
+    plt.show()

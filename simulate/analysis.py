6f7c57c7650ce2fa1fc673c5198b9601a6d68086
Zac Chan <sunshinecltzac@gmail.com>
2018-04-23 21:54:10 +0800

    simulate analysis bug fix

--- simulate/analysis.py
+++ simulate/analysis.py
@@ -11,10 +11,9 @@ from utils.savitzky_golay import savitzky_golay
 def plot_and_calculate(data, data_name):
     data = np.array(data)
 
-    randomized_data = data[::2, :]
-    mean = np.mean(randomized_data, axis=0)
+    mean = np.mean(data, axis=0)
     mean = savitzky_golay(mean, 25, 1)
-    std = np.std(randomized_data, axis=0)
+    std = np.std(data, axis=0)
     plt.plot(mean, linewidth=3.0, label="mean")
     plt.fill_between(range(0, TRIAL_LENGTH), mean - 2 * std, mean + 2 * std, alpha=0.2)
     for participant in range(0, NUMBER_OF_PARTICIPANT, 2):
@@ -28,27 +27,7 @@ def plot_and_calculate(data, data_name):
         plt.ylim([0, 50])
     else:
         plt.ylim([0, 4])
-    plt.title(data_name + " under randomized condition")
-    plt.show()
-
-    block_data = data[1::2, :]
-    mean = np.mean(block_data, axis=0)
-    mean = savitzky_golay(mean, 25, 1)
-    std = np.std(block_data, axis=0)
-    plt.plot(mean, linewidth=3.0, label="mean")
-    plt.fill_between(range(0, TRIAL_LENGTH), mean - 2 * std, mean + 2 * std, alpha=0.2)
-    for participant in range(1, NUMBER_OF_PARTICIPANT, 2):
-        plt.plot(savitzky_golay(data[participant], 25, 1), linewidth=0.5, label=str(participant))
-    plt.vlines(36, 0, 30)
-    plt.vlines(72, 0, 30)
-    plt.vlines(108, 0, 30)
-    if data_name == "step":
-        plt.ylim([0, 30])
-    elif data_name == "time":
-        plt.ylim([0, 50])
-    else:
-        plt.ylim([0, 4])
-    plt.title(data_name + " under block condition")
+    plt.title(data_name + " under " + ("randomized" if randomized else "block") + " condition")
     plt.show()
 
     print("All ", data_name, ":")
@@ -56,18 +35,6 @@ def plot_and_calculate(data, data_name):
     print(data.mean(axis=0)[36:72].mean())
     print(data.mean(axis=0)[72:108].mean())
     print(data.mean(axis=0)[108:].mean())
-
-    print("Randomized", data_name, ":")
-    print(randomized_data.mean(axis=0)[:36].mean())
-    print(randomized_data.mean(axis=0)[36:72].mean())
-    print(randomized_data.mean(axis=0)[72:108].mean())
-    print(randomized_data.mean(axis=0)[108:].mean())
-
-    print("Block", data_name, ":")
-    print(block_data.mean(axis=0)[:36].mean())
-    print(block_data.mean(axis=0)[36:72].mean())
-    print(block_data.mean(axis=0)[72:108].mean())
-    print(block_data.mean(axis=0)[108:].mean())
 
 
 if __name__ == "__main__":

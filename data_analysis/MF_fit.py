3547f12a545aabf5b695f6841841cd9ba2c23d33
Zac Chen <sunshinecltzac@gmail.com>
2018-05-03 01:11:56 +0800

    MF fit bug fix

--- data_analysis/MF_fit.py
+++ data_analysis/MF_fit.py
@@ -2,21 +2,17 @@ import csv
 import datetime
 import json
 import os
-import random
-import shutil
 
 import numpy as np
-from psychopy import data
 from scipy import optimize
 
-from Transition import Transition
 from utils import utils
 
 
-def MF_lld(alpha=0.1,
-           tau=1,
-           gamma=0.9,
-           randomized=True):
+def MF_lld(params):
+    alpha = params[0]
+    tau = params[1]
+    gamma = params[2]
 
     # model and parameters
     q_value = np.zeros(shape=[3, 6, 3])
@@ -24,7 +20,7 @@ def MF_lld(alpha=0.1,
     lld = 0
 
     for episode in range(144):
-        trial_end_state = trials_data[episode][-1][0]
+        trial_end_state = trials_data[episode][-1][2]
 
         step = 0
         last_state = 0
@@ -34,11 +30,14 @@ def MF_lld(alpha=0.1,
             action = transit[1]
             step += 1
 
-            lld -= np.log(utils.softmax(np.array([q_value[trial_end_state][now_state, 0],
-                                                  q_value[trial_end_state][now_state, 1],
-                                                  q_value[trial_end_state][now_state, 2]]),
-                                        tau)
-                          [action])
+            likelihood = utils.softmax(np.array([q_value[trial_end_state][now_state, 0],
+                                                 q_value[trial_end_state][now_state, 1],
+                                                 q_value[trial_end_state][now_state, 2]]),
+                                       tau)[action]
+            if likelihood < 1e-200:
+                lld += 460.517
+            else:
+                lld -= np.log(likelihood)
 
             if step != 1:
                 target = 0 + gamma * q_value[trial_end_state][now_state, action]
@@ -50,8 +49,8 @@ def MF_lld(alpha=0.1,
             q_value *= 0.99
 
         target = max(21 - step, 1)
-        delta = target - q_value[trial_end_state][last_state, last_state]
-        q_value[trial_end_state][last_state, last_state] += alpha * delta
+        delta = target - q_value[trial_end_state][last_state, last_action]
+        q_value[trial_end_state][last_state, last_action] += alpha * delta
 
     return lld
 
@@ -81,7 +80,7 @@ if __name__ == "__main__":
                 length.append(len(transformed_trial))
         trials_data = trials_data[:TRIAL_LENGTH]
 
-        result = optimize.minimize(MF_lld, np.array([0.1, 0.5, 0.9]), (participant_id % 2 == 0,))
+        result = optimize.minimize(MF_lld, np.array([0.1, 0.5, 0.9]), bounds=[(0, 1), (1e-200, None), (0, 1)])
         print("For participant %d, best fit lld is %.3f, alpha=%.2f, tau=%.2f, gamma=%.2f" %
               (participant_id, result.fun, result.x[0], result.x[1], result.x[2]))
 

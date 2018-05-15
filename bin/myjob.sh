ccab37e7c152b9fa1ec422bb0184140c7f4f4be4
Zac Chan <sunshinecltzac@gmail.com>
2018-05-15 22:21:19 +0800

    bug fix

--- bin/myjob.sh
+++ bin/myjob.sh
@@ -15,5 +15,5 @@
 module load anaconda/3-4.4.0.1
 source activate lab
 export PYTHONPATH=/gpfs/share/home/1400013706/RLpsy:${PYTHONPATH}
-
-python data_analysis/MF_fit.py
+cd ~/RLpsy/data_analysis
+python MF_fit.py

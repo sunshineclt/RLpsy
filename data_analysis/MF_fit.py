58258726067a18f4c4bca56ef2d90603a7fca31a
Zac Chan <sunshinecltzac@gmail.com>
2018-05-15 21:13:06 +0800

    organize MF_fit code

--- data_analysis/MF_fit.py
+++ data_analysis/MF_fit.py
@@ -9,6 +9,7 @@ import numpy as np
 from scipy import optimize
 
 from utils import utils
+import utils.Params as Params
 from utils.draw import draw_participant_and_simulation
 from simulate.simulate_MF import simulate_MF
 from data_analysis.analysis_optimal import optimal_probability
@@ -19,56 +20,55 @@ def MF_lld(params):
     tau = params[1]
     gamma = params[2]
     forget = params[3]
-    # alpha_outer = params[4]
 
-    # model and parameters
     q_value = np.zeros(shape=[3, 6, 3])
 
     lld = 0
-
-    for episode in range(144):
+    for episode in range(TRIAL_LENGTH):
         trial_end_state = trials_data[episode][-1][2]
 
         step = 0
-        # last_state = 0
-        # last_action = 0
+        if not Q_LEARNING:
+            last_state = 0
+            last_action = 0
         for transit in trials_data[episode]:
             now_state = transit[0]
-            action = transit[1]
+            action_chosen = transit[1]
             step += 1
 
             likelihood = utils.softmax(np.array([q_value[trial_end_state][now_state, 0],
                                                  q_value[trial_end_state][now_state, 1],
                                                  q_value[trial_end_state][now_state, 2]]),
-                                       tau)[action]
+                                       tau)[action_chosen]
             if likelihood < 1e-200:
                 lld += 460.517
             else:
                 lld -= np.log(likelihood)
 
-            # if step != 1:
-            #     target = 0 + gamma * q_value[trial_end_state][now_state, action]
-            #     delta = target - q_value[trial_end_state][last_state, last_action]
-            #     q_value[trial_end_state][last_state, last_action] += alpha * delta
-            new_state = transit[2]
-            if trial_end_state == new_state:
-                target = max(21 - step, 1)
-            else:
-                target = gamma * np.max(q_value[trial_end_state][new_state])
-            delta = target - q_value[trial_end_state][now_state, action]
-            if now_state > 2:
-                q_value[trial_end_state][now_state, action] += alpha * delta
-            else:
-                q_value[trial_end_state][now_state, action] += alpha * delta
+            if Q_LEARNING:
+                new_state = transit[2]
+                if trial_end_state == new_state:
+                    target = max(21 - step, 1)
+                else:
+                    target = gamma * np.max(q_value[trial_end_state][new_state])
+                delta = target - q_value[trial_end_state][now_state, action_chosen]
+                q_value[trial_end_state][now_state, action_chosen] += alpha * delta
+            elif step != 1:
+                target = 0 + gamma * q_value[trial_end_state][now_state, action_chosen]
+                delta = target - q_value[trial_end_state][last_state, last_action]
+                q_value[trial_end_state][last_state, last_action] += alpha * delta
 
-            # last_state = now_state
-            # last_action = action
+            if not Q_LEARNING:
+                last_state = now_state
+                last_action = action_chosen
+
+            # forget
             q_value *= (1 - forget)
-            # print(q_value[2][4])
 
-        # target = max(21 - step, 1)
-        # delta = target - q_value[trial_end_state][last_state, last_action]
-        # q_value[trial_end_state][last_state, last_action] += alpha * delta
+        if not Q_LEARNING:
+            target = max(21 - step, 1)
+            delta = target - q_value[trial_end_state][last_state, last_action]
+            q_value[trial_end_state][last_state, last_action] += alpha * delta
 
     return lld
 
@@ -79,6 +79,7 @@ if __name__ == "__main__":
 
     NUMBER_OF_PARTICIPANT = 36
     TRIAL_LENGTH = 144
+    Q_LEARNING = True
 
     MF_fit_result = open("MF_fit_result.csv", "w")
     fieldnames = ["participant",
@@ -110,38 +111,38 @@ if __name__ == "__main__":
 
         optimal_analysis_result = optimal_probability(participant_id, trials_data, is_simulate=False)
 
-        # MF_lld([0.1, 1, 0.9, 0.001])
-        # break
+        # model fitting
         def minimize(args):
             f, x, bound = args
             res = optimize.minimize(f, x, bounds=bound)
             return res
 
         pool = mp.Pool(12)
-        bounds = [(0, 1), (1e-5, 100), (0, 1), (0, 0.05), (0, 1)]
-        initial_value = []
-        for i in range(12):
-            initial_value.append(np.array([np.random.random(),
-                                           np.random.random()*100+1e-5,
-                                           np.random.random(),
-                                           np.random.random()*0.05,
-                                           np.random.random()]))
+        bounds = [Params.PARAM_BOUNDS["alpha"],
+                  Params.PARAM_BOUNDS["tau"],
+                  Params.PARAM_BOUNDS["gamma"],
+                  Params.PARAM_BOUNDS["forget_MF"]]
+        NUMBER_OF_INITIAL_VALUE = 1000
+        initial_value = np.zeros(shape=[NUMBER_OF_INITIAL_VALUE, len(bounds)])
+        for i in range(NUMBER_OF_INITIAL_VALUE):
+            for bound_index, bound in enumerate(bounds):
+                initial_value[i, bound_index] = np.random.random() * (bound[1] - bound[0]) + bound[0]
 
-        condition = [(MF_lld, initial[:4], bounds[:4]) for initial in initial_value]
+        condition = [(MF_lld, initial, bounds) for initial in initial_value]
 
         result = pool.map(minimize, condition)
         min_fun = 1e50
-        min_fun_x = 0
+        min_fun_x = -1
         for i in range(len(initial_value)):
             if result[i].fun < min_fun:
                 min_fun = result[i].fun
                 min_fun_x = result[i].x
 
-        # result = optimize.minimize(MF_lld, np.array([0.1, 1, 0.9, 0.001, 0.1][:4]), bounds=[(0, 1), (1e-5, 100), (0, 1), (0, 0.05), (0, 1)][:4])
         print("For participant %d, best fit lld is %.3f, alpha=%.2f, tau=%.2f, gamma=%.2f, forget=%.5f" %
               (participant_id, min_fun, *min_fun_x))
         writer.writerow(dict(zip(fieldnames, [participant_id, min_fun, *min_fun_x])))
 
+        # Use the fitted model to simulate
         # simulate_MF(randomized=participant_id % 2 == 0,
         #             alpha=result.x[0],
         #             tau=result.x[1],
---
+++ utils/Params.py
@@ -0,0 +1,11 @@
+
+
+PARAMS = ["alpha", "eta", "tau", "gamma", "forget_MF", "forget_MB", "I", "k"]
+PARAM_BOUNDS = {"alpha": (0, 1),
+                "eta": (0, 1),
+                "tau": (1e-5, 100),
+                "gamma": (0, 1),
+                "forget_MF": (0, 0.05),
+                "forget_MB": (0, 0.05),
+                "I": (0, 1),
+                "k": (0, 0.1)}

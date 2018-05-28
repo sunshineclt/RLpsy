ff4d9d7da45e17eb3198209e464d5d1321472c50
Zac Chan <sunshinecltzac@gmail.com>
2018-05-28 15:01:25 +0800

    bug fix

--- data_analysis/simulate_analysis_fitted_params.py
+++ data_analysis/simulate_analysis_fitted_params.py
@@ -26,14 +26,14 @@ for participant_param in fit_result:
     participant_id = int(participant_param["participant"])
     # Use the fitted model to simulate
     BASE_PATH = "simulate_data/%s/%02d/" % (MODEL_NAME, participant_id)
-    simulate_MF(randomized=participant_id % 2 == 0,
-                alpha=float(participant_param["alpha"]),
-                tau=float(participant_param["tau"]),
-                repeat=NUMBER_OF_REPEAT,
-                gamma=float(participant_param["gamma"]),
-                forget=float(participant_param["forget_MF"]),
-                path=BASE_PATH,
-                seed=participant_id)
+    # simulate_MF(randomized=participant_id % 2 == 0,
+    #             alpha=float(participant_param["alpha"]),
+    #             tau=float(participant_param["tau"]),
+    #             repeat=NUMBER_OF_REPEAT,
+    #             gamma=float(participant_param["gamma"]),
+    #             forget=float(participant_param["forget_MF"]),
+    #             path=BASE_PATH,
+    #             seed=participant_id)
     # simulate_MB(randomized=participant_id % 2 == 0,
     #             eta=float(participant_param["eta"]),
     #             tau=float(participant_param["tau"]),
@@ -66,5 +66,5 @@ for participant_param in fit_result:
         savers[repeat_id].save_trials_data("optimal_outer", participant_id, result[1]["outer"])
         savers[repeat_id].save_trials_data("optimal_last", participant_id, result[1]["last"])
 
-    for repeat in range(NUMBER_OF_REPEAT):
-        savers[repeat].save_to_file("simulate_analysis_result/" + MODEL_NAME + "_simulate_analysis_result.pkl")
+for repeat in range(NUMBER_OF_REPEAT):
+    savers[repeat].save_to_file("simulate_analysis_result/" + MODEL_NAME + "_simulate_analysis_result" + str(repeat) + ".pkl")

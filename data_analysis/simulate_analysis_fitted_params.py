9e128548c1ae954ebf0c01f8e1b319f08748c74d
Zac Chan <sunshinecltzac@gmail.com>
2018-05-28 11:35:28 +0800

    bug fix

--- data_analysis/simulate_analysis_fitted_params.py
+++ data_analysis/simulate_analysis_fitted_params.py
@@ -7,43 +7,47 @@ from simulate.simulate_MB import simulate_MB
 from utils.DataSaver import DataSaver
 from data_analysis.analysis_optimal import optimal_probability
 
-MODEL_NAME = "MB"
+MODEL_NAME = "MF"
 
 f = open(MODEL_NAME + "_fit_result.csv", "r")
 fit_result = csv.DictReader(f)
 
 NUMBER_OF_PARTICIPANT = 36
 TRIAL_LENGTH = 144
-saver = DataSaver(["optimal",
-                   "optimal_inner", "optimal_outer", "optimal_last"],
-                  NUMBER_OF_PARTICIPANT,
-                  TRIAL_LENGTH)
+NUMBER_OF_REPEAT = 50
+savers = []
+for _ in range(NUMBER_OF_REPEAT):
+    savers.append(DataSaver(["optimal",
+                             "optimal_inner", "optimal_outer", "optimal_last"],
+                            NUMBER_OF_PARTICIPANT,
+                            TRIAL_LENGTH))
 
 for participant_param in fit_result:
     participant_id = int(participant_param["participant"])
     # Use the fitted model to simulate
     BASE_PATH = "simulate_data/%s/%02d/" % (MODEL_NAME, participant_id)
-    # simulate_MF(randomized=participant_id % 2 == 0,
-    #             alpha=float(participant_param["alpha"]),
+    simulate_MF(randomized=participant_id % 2 == 0,
+                alpha=float(participant_param["alpha"]),
+                tau=float(participant_param["tau"]),
+                repeat=NUMBER_OF_REPEAT,
+                gamma=float(participant_param["gamma"]),
+                forget=float(participant_param["forget_MF"]),
+                path=BASE_PATH,
+                seed=participant_id)
+    # simulate_MB(randomized=participant_id % 2 == 0,
+    #             eta=float(participant_param["eta"]),
     #             tau=float(participant_param["tau"]),
     #             repeat=1,
     #             gamma=float(participant_param["gamma"]),
-    #             forget=float(participant_param["forget_MF"]),
+    #             forget=float(participant_param["forget_MB"]),
     #             path=BASE_PATH,
     #             seed=participant_id)
-    simulate_MB(randomized=participant_id % 2 == 0,
-                eta=float(participant_param["eta"]),
-                tau=float(participant_param["tau"]),
-                repeat=1,
-                gamma=float(participant_param["gamma"]),
-                forget=float(participant_param["forget_MB"]),
-                path=BASE_PATH,
-                seed=participant_id)
-    NUMBER_OF_SIMULATION = 1
 
     for file in os.listdir(BASE_PATH):
         path = os.path.join(BASE_PATH, file)
         print("Loading ", path)
+        split = file.split("_")
+        repeat_id = int(split[0])
 
         rawFile = open(path, "r")
         reader = csv.DictReader(rawFile, delimiter="#")
@@ -57,9 +61,10 @@ for participant_param in fit_result:
 
         result = optimal_probability(participant_id, trials_data, is_simulate=True,
                                      is_randomized=participant_id % 2 == 0)
-        saver.save_trials_data("optimal", participant_id, result[0])
-        saver.save_trials_data("optimal_inner", participant_id, result[1]["inner"])
-        saver.save_trials_data("optimal_outer", participant_id, result[1]["outer"])
-        saver.save_trials_data("optimal_last", participant_id, result[1]["last"])
+        savers[repeat_id].save_trials_data("optimal", participant_id, result[0])
+        savers[repeat_id].save_trials_data("optimal_inner", participant_id, result[1]["inner"])
+        savers[repeat_id].save_trials_data("optimal_outer", participant_id, result[1]["outer"])
+        savers[repeat_id].save_trials_data("optimal_last", participant_id, result[1]["last"])
 
-    saver.save_to_file(MODEL_NAME + "_simulate_analysis_result.pkl")
+    for repeat in range(NUMBER_OF_REPEAT):
+        savers[repeat].save_to_file("simulate_analysis_result/" + MODEL_NAME + "_simulate_analysis_result.pkl")
